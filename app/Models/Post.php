c384d01ad223f9d25aded275f78cfe5deb177dbf
eanp <awifamz@gmail.com>
2025-02-20 00:54:01 +0700

    setup project

---
+++ app/Models/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title',
+        'slug',
+        'content',
+        'excerpt',
+        'featured_image',
+        'user_id',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+}
